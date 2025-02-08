c22b82ddee6032110153cb3540585797c9bd647c
Davitrons <davitrons@gmail.com>
2025-02-08 19:47:04 +0100

    Ejercicio 4: Tabla de Multiplicar (Bucle Anidado for)

--- scripts/script.js
+++ scripts/script.js
@@ -30,3 +30,11 @@ function clasificadorEdades(num = 25){
         console.log(`Eres un adulto mayor.`);
     }
 }
+
+function tablaMultiplicar(){
+    for (let i = 1; i <= 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+        console.log(`${i} * ${j} = ${i * j}`);
+        }
+    }
+}
